a45f092fe71484dff44e02a006617b2d175e18b5
null null <BitShopHelp@yahoo.com>
2020-05-27 21:12:14 +0000

    Update Templates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main():
+def default():
   return render_template("default.html")
 
 app.run(host='0.0.0.0', port=8080)
