015758260b5ae9d07b05ccabc29239eba7c0d687
null null <BitShopHelp@yahoo.com>
2020-05-27 20:05:31 +0000

    Add template engine

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def main():
+  return render_template("default.html")
 
 app.run(host='0.0.0.0', port=8080)
